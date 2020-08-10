f2b4e63484ac2095df87934b7085a532ed631e8f
John Howland <>
2020-08-09 21:37:56 -0400

    Error in hardware_interface file. Fixed method declarations, and variables.

--- hardware_interface.py
+++ hardware_interface.py
@@ -5,34 +5,35 @@ import gpiozero
 
 ON = 1
 OFF = 0
-sim_mode = False
 
 class pin_class(object):
 	def __init__(self, LED_number):
 		self.led_state = OFF
 		if os.name == 'nt':
 			print("On a windows system")
-			sim_mode = True
+			self.sim_mode = True
+			self.led = 0
 		else:
 			self.led = gpiozero.LED(int(LED_number))
 			print("Not on a windows system")
+			self.sim_mode = False
 
 		#gpiozero.Device.pin_factory
 		pass
 
-	def setPinState(state):
+	def setPinState(self, state):
 		if state == ON:
 			self.led_state = ON
-			if sim_mode is False:
+			if self.sim_mode is False:
 				self.led.on()
 		elif state == OFF:
 			self.led_state = OFF
-			if sim_mode is False:
+			if self.sim_mode is False:
 				self.led.off()
 		else:
 			print("How did you get there?!")
 
-	def getPinState():
+	def getPinState(self):
 		if self.led_state == ON:
 			print("LED status is ON")
 		elif self.led_state == OFF:
